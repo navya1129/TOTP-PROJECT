17aa22e3a72e07434929b3b70ed0e82fe2c9a79f
Navya Rayi <23A91A1252@aec.edu.in>
2025-12-04 20:53:45 +0530

    change

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, HTTPException
-import pyotp
 import time
 import os
 
-from app.crypto_utils import decrypt_seed
-
+from crypto_utils import (
+    decrypt_seed,
+    generate_totp_code,
+    verify_totp_code
+)
 
 app = FastAPI()
 
 DATA_PATH = "/data/seed.txt"
+PRIVATE_KEY_PATH = "/add/student_verify_private"   # very important
+
 
 # ------------------------
 # POST /decrypt-seed
@@ -18,9 +22,9 @@ def decrypt_seed_api(body: dict):
     if "encrypted_seed" not in body:
         raise HTTPException(400, "Missing encrypted_seed")
 
-    seed_hex = decrypt_seed(body["encrypted_seed"])
-
-    if seed_hex is None:
+    try:
+        seed_hex = decrypt_seed(body["encrypted_seed"], PRIVATE_KEY_PATH)
+    except Exception as e:
         raise HTTPException(500, "Decryption failed")
 
     os.makedirs("/data", exist_ok=True)
@@ -28,6 +32,7 @@ def decrypt_seed_api(body: dict):
         f.write(seed_hex)
 
     return {"status": "ok"}
+
 
 # ------------------------
 # GET /generate-2fa
@@ -40,14 +45,14 @@ def generate_2fa():
     with open(DATA_PATH, "r") as f:
         seed_hex = f.read().strip()
 
-    secret = pyotp.TOTP(seed_hex)
-    code = secret.now()
+    code = generate_totp_code(seed_hex)
     remaining = 30 - (int(time.time()) % 30)
 
     return {
         "code": code,
         "valid_for": remaining
     }
+
 
 # ------------------------
 # POST /verify-2fa
@@ -63,10 +68,6 @@ def verify_2fa(body: dict):
     with open(DATA_PATH, "r") as f:
         seed_hex = f.read().strip()
 
-    totp = pyotp.TOTP(seed_hex)
-
-    valid = (
-        totp.verify(body["code"], valid_window=1)
-    )
+    valid = verify_totp_code(seed_hex, body["code"])
 
     return {"valid": valid}
